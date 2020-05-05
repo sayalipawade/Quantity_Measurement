6218fc4bb36cfd7124eeb506b33e34c82f6f4bbe
sayalipawade <sayalipawade05@gmail.com>
2020-05-05 11:31:34 +0530

    Added:Welcome message

---
+++ src/main/java/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package quantitymeasurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity measurement");
+    }
+}
